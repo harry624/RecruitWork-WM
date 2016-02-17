09a5ba34aa8888e3f07eb3cd51a28141d4bba41a
harrywang624 <harrywang624@gmail.com>
2016-02-17 22:18:59 +0800

    first commit

---
+++ ZHDaily/ZHDaily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHDaily
+//
+//  Created by 王豪 on 16/2/17.
+//  Copyright © 2016年 hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHDaily/ZHDaily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZHDaily
+//
+//  Created by 王豪 on 16/2/17.
+//  Copyright © 2016年 hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
